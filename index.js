c109f8d26bf62bdc726b4de5a0fee0f5abab8215
otatatataspin <yuriabeizhong@gmail.com>
2021-12-21 23:39:38 +0900

    allow localhost3000

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+const fs = require("fs");
+const path = require("path");
+const pathToFile = path.resolve("./data.json");
+const cors = require("cors");
+
+const corsOption = {
+    origin: "http://localhost:3000",
+    optionStatus: 200,
+}
+app.use(cors(corsOption))
+
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send(pathToFile)
 })
 
 app.get("/api/resources", (req, res) => {
-    res.send("Hello Resources")
+    const resources = JSON.parse(fs.readFileSync(pathToFile))
+    res.send(resources)
 })
 
 app.listen(PORT, () => {
